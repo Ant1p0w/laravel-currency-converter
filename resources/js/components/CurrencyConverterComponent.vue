cac46ca779ca9bb3aab9070f7c2a41e2dda540a1
Antipow <antipow@gmail.com>
2020-07-23 00:11:44 +0300

    add empty vue component

---
+++ resources/js/components/CurrencyConverterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div></div>
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+
+            }
+        },
+        mounted: function () {
+
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+    }
+</script>
+<style>
+
+</style>
