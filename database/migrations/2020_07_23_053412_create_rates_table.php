8aa5f79e72b0f42f9145cf407e314fcb5c098479
Antipow <antipow@gmail.com>
2020-07-23 08:44:54 +0300

    add rate model

--- database/migrations/2020_07_23_053412_create_rates_table.php
+++ database/migrations/2020_07_23_053412_create_rates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCurrenciesTable extends Migration
+class CreateRatesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreateCurrenciesTable extends Migration
      */
     public function up()
     {
-        Schema::create('currencies', function (Blueprint $table) {
+        Schema::create('rates', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->bigInteger('currency_id')->unsigned();
+            $table->float('rate', 8, 2);
             $table->timestamps();
+            $table->foreign('currency_id')->references('id')->on('currencies')
+                ->onDelete('cascade');
         });
     }
 
@@ -27,6 +30,6 @@ class CreateCurrenciesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('currencies');
+        Schema::dropIfExists('rates');
     }
 }
